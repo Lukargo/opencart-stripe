29bf9f5f56d74ed3b56ddac87538333873b15921
Chad Wilken <chad.wilken@gmail.com>
2012-09-04 21:53:27 -0500

    Added template and payment files

--- upload/admin/controller/payment/stripe.php
+++ upload/admin/controller/payment/stripe.php
@@ -1,9 +1,9 @@
 <?php 
-class ControllerPaymentAuthorizenetAim extends Controller {
+class ControllerPaymentStripe extends Controller {
 	private $error = array(); 
 
 	public function index() {
-		$this->load->language('payment/authorizenet_aim');
+		$this->load->language('payment/stripe');
 
 		$this->document->setTitle($this->language->get('heading_title'));
 		
